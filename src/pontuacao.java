a81c05058f0a8048cc165d01f556ea6633f4f545
Luiz Felipe <luizdoid.fl@hotmail.com>
2020-05-15 19:34:46 -0300

    Leitura do volume

--- src/pontuacao.java
+++ src/pontuacao.java
@@ -7,11 +7,25 @@ public class pontuacao {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
 		
+		int quantidadedecompras;
+		double ticketmedio;
+		
+		
+		// CABEÇALHO //
 		
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		System.out.println();	
 
+		// LEITURA DO VOLUME //
+		
+		System.out.printf("Quantas compras o cliente fez no último ano?");
+		quantidadedecompras = sc.nextInt();
+		System.out.printf("Qual o ticket médio?");
+		ticketmedio = sc.nextDouble();
+
+		
 		sc.close();
 	}
 
