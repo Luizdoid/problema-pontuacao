359588bf497551cb4764cbf101f017797d3a896d
Luiz Felipe <luizdoid.fl@hotmail.com>
2020-05-15 19:19:58 -0300

    Cabecalho

--- src/pontuacao.java
+++ src/pontuacao.java
@@ -6,7 +6,11 @@ public class pontuacao {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
-
+		
+		
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("--------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
 
 		sc.close();
 	}
